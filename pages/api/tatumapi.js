02c7cedefe405704a624dddc6deb6283a0bfdc12
luis.poveda93 <luis.poveda9321@gmail.com>
2022-06-11 21:53:28 +0200

    testing api

---
+++ pages/api/tatumapi.js
@@ -0,0 +1,17 @@
+export default async (req, res) => {
+    const chain = 'ETH';
+    const address = '0x8926d4fDF7D1Ca0bB7d803f143fE2036245c57be';
+    const URL = `https://api-eu1.tatum.io/v3/nft/address/balance/${chain}/${address}`;
+    const response = await fetch(
+        URL,
+        {
+            method: 'GET',
+            headers: {
+              'x-api-key': `${process.env.TATUM_API_KEY}`
+            }
+          }
+    );
+  
+    const data = await response.text();
+    console.log(data);
+}
