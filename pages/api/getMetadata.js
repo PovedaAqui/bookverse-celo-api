bfe6f55999ad23afb2a1065f35f220a8c10296b1
luis.poveda93 <luis.poveda9321@gmail.com>
2022-07-03 03:24:53 +0200

    api added

---
+++ pages/api/getMetadata.js
@@ -0,0 +1,34 @@
+import Cors from 'cors';
+import initMiddleware from '../../lib/init-middleware';
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
+export default async (req, res) => {
+
+    // Run cors
+    await cors(req, res)
+
+    const chain = 'CELO';
+    const nftAddress = req.body.nftAddress;
+    const tokenId = req.body.tokenId;
+    console.log(tokenId);
+    const URL = `https://api-eu1.tatum.io/v3/nft/address/balance/${chain}/${nftAddress}/${tokenId}`;
+    const response = await fetch(
+        URL,
+        {
+            method: 'GET',
+            headers: {
+              'x-api-key': `${process.env.TATUM_API_KEY}`
+            }
+          }
+    )
+    .then(response => response.json())
+    .then(data => res.status(200).json({ data: data}))
+}
