2c289238a75042892998c92104a8427c626cd2b9
luis.poveda93 <luis.poveda9321@gmail.com>
2022-07-05 23:58:55 +0200

    api updated

--- pages/api/getMetadata.js
+++ pages/api/getMetadata.js
@@ -18,7 +18,7 @@ export default async (req, res) => {
     const chain = 'CELO';
     const {nftAddress, tokenId} = req.body;
     console.log(tokenId);
-    const URL = `https://api-eu1.tatum.io/v3/nft/address/balance/${chain}/${nftAddress}/${tokenId}`;
+    const URL = `https://api-eu1.tatum.io/v3/nft/metadata/${chain}/${nftAddress}/${tokenId}`;
     const response = await fetch(
         URL,
         {
